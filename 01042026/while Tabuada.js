1856f9422c1069c4cc4c7d91176c0d8333a4ea4b
Luis.Rezer <lc.rezer@bol.com.br>
2026-04-15 21:48:51 -0300

    atualizar

--- 01042026/while Tabuada.js
+++ 01042026/while Tabuada.js
@@ -1,4 +1,3 @@
 // tabuada com while
 while (let i = 1; <=10; i++) {
     console.log ('n=== tabuada do ${i}===');
-    
---
+++ 14042026 PI/Projeto Integrador.js
@@ -0,0 +1,11 @@
+// Tema Inclusao 
+// Solucao para ajudar pais que tem filhos com TDH;
+// Software deve conter BOTAO:
+// Sinais;
+// Diagnostico;
+// Onde Buscar ajuda;
+// tutoriais de auxilio para escola;
+// tutoriais de auxilio para dia a dia;
+// integracao com watsap que envolve grupos de pais que tem esta vivencia.
+
+
